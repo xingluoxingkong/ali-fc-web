0f5a5e0b98f8688826783bb237af0d8dd9783eaa
unknown <ljemail@dingtalk.com>
2019-11-01 14:27:52 +0800

    1101

--- test/application.py
+++ test/application.py
@@ -1,5 +1,5 @@
 conf_center = {
-        'url': 'https://1921668875657123.cn-zhangjiakou.fc.aliyuncs.com/2016-08-15/proxy/ly-config/config/', 
-        'pwd': 'WCeHihjdLKoV'
+        'url': 'config/config/', 
+        'pwd': ''
     }
 sql_name = 'sql'
