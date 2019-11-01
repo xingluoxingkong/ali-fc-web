4e77d75365ed4f34ed575ff5da6dce75fdd2349f
unknown <ljemail@dingtalk.com>
2019-11-01 14:26:46 +0800

    1101

--- conf/application.py
+++ conf/application.py
@@ -1,26 +1,26 @@
 # 密码
-pwd = 'WCeHihjdLKoV'
+pwd = ''
 
 # mysql配置
 sql = {
-	"url": "39.100.80.80",
-	"database": "guizhoulvyou",
-	"username": "mengyou",
-	"password": "Mengyou@5678",
+	"url": "",
+	"database": "",
+	"username": "",
+	"password": "",
 	"charset": "utf8"
 }
 
 # redis配置
 redis = {
-	"host": "39.100.80.80",
+	"host": "",
 	"port": 6379,
-	"password": "123456"
+	"password": ""
 }
 
 # 微信小程序客户端配置
 wx = {
-	"appid": "wx6e784839301a2316",
-	"secret": "3b0aaab7141f4da8989875a464407655"
+	"appid": "",
+	"secret": ""
 }
 
 # 通联支付配置
@@ -33,56 +33,23 @@ allin = {
 
 # 导游端小程序配置
 wx_guide = {
-	"appid": "wxfdda837628ed295e",
-	"secret": "b7deedfa91b7d1250af77f1b07fa59d6",
-	"token": "26_anTg9qTj9oRMbLXE6SdU1RfGgt1cnITjRbbz7NRoXTSwImdKAy4UWXnI_3ZdfzPdqHURYO5RjOMSfzTwkv2cDs7usAqPAhoPZUs0ZxoqFm25xQXK20f5jnKy9wKF6yDiBugYHi3numPn9GqbGOGcAJAZHZ"
+	"appid": "",
+	"secret": ""
 }
 
 # 阿里云配置
 aliyun = {
-	"UID": "221318170847266307",
-	"AccessKeyID": "LTAI4FiC3BRcbgawMDfnM2BA",
-	"AccessKeySecret": "qbxD3lcfXSFjit1QTgRP8tsikWHhub",
-	"SignName": "萌游科技",
-	"TemplateCode": "SMS_175539742"
+	"UID": "",
+	"AccessKeyID": "",
+	"AccessKeySecret": "",
+	"SignName": "",
+	"TemplateCode": ""
 }
 
 # 公钥
 rsa_public_key = '''-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5bTkh2e88fGy5vKrX5Pf
-xalQuJAB18hPsTt1vLpgagljbK9kD9BaoE/h9LVuGEhTpNMqOjhPbVCfn3031Wev
-mWNoMBI0parE9C0vaKeTktqjahi7tdNPnHguvTQvJWuKG5kYksHD1+KNrgZlR4OO
-v+BjNuYQk+VvtTCTmpUsuP+UGRv7CGBQLyXF6vSGzJ4ZpWok9GBELaznQn1OCan1
-i01m9X+f7/iydSAJXLIr0wp5/Jn2zRoY+CV+MNXb2Wl02f38ZdqsGm2Cc3vW4q9B
-0bcXMsnBwhd6ovehPkx1DKStiGGQN7xSDM/1Lob9bebf7BlucPzTOJiFlszSGwxE
-YQIDAQAB
 -----END PUBLIC KEY-----'''
 
 # 私钥
 rsa_private_key = '''-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEA5bTkh2e88fGy5vKrX5PfxalQuJAB18hPsTt1vLpgagljbK9k
-D9BaoE/h9LVuGEhTpNMqOjhPbVCfn3031WevmWNoMBI0parE9C0vaKeTktqjahi7
-tdNPnHguvTQvJWuKG5kYksHD1+KNrgZlR4OOv+BjNuYQk+VvtTCTmpUsuP+UGRv7
-CGBQLyXF6vSGzJ4ZpWok9GBELaznQn1OCan1i01m9X+f7/iydSAJXLIr0wp5/Jn2
-zRoY+CV+MNXb2Wl02f38ZdqsGm2Cc3vW4q9B0bcXMsnBwhd6ovehPkx1DKStiGGQ
-N7xSDM/1Lob9bebf7BlucPzTOJiFlszSGwxEYQIDAQABAoIBAFDpqDoeC4s59Seu
-wTj4kLFCk+F/MHb+4ztt02o4SG/xgKkvGtMLNHEV0mTtZ6t8BqKfk1dQsIG0DZYT
-eJBeZpdkUbXlI9BbV+ce53040h2/kCJ2NeC1X2yhVO1mrVjECbDw+5vuTvsDnZ54
-OEK5j5X9B1W2Mo62GeQ8CTJBa7B6A4JaHJtQYiF+XPc3NB1PxXqscrqAuC+KT8oL
-bDTs1Jh8WVt2gL765WUd7FX+zHoCa8sZBUYPTcw5ogC04N5ziMMOpMgL8yRPQW0b
-P/e++yNre1Cc+Qf/JDsNX8Zfqq9qbUEv7hc9nCBSh4S+2q+Lg4Z0lfcDtILfX3WM
-9l13x3ECgYEA/XVR678ChRjvDLeDWKSI8H7iuo1CN9mCESN5L6VlK9FuV9+iLJV8
-qzUYUMYBo7ZdacYNS1g3X/tPie+eYB3JnPEp+1+oJ515Rbva+9Iw4p5Sk1PrGXH5
-MTGEQLAwkE55L0wMpPAEp7E368bNjiYuGT7rcCXNWN9sONdTflzEo/0CgYEA6AKY
-47j1q2T83VtzczGcGXnmeE88oA9AgGVPTWsOFstTpOJBm7Pek3y4Ha5UrwE96nsj
-yu10cIjNUvGeMtPC+8b9XpQIztnhFrADc4LcFxINBbRwNeGZUmPkhlb0yp188A0O
-Kk+J8IhX+Yh6fHYKSzqirteakBxLEcC+mpbJ5TUCgYATOJRv1lpwkYV/Z2NUk5W2
-90hmcVP2PE560j/cdySHACjoDROVYfjEWxBu/7UcHzZSUSys7BG2+636fet6yoeB
-mWBfqc9swapyuV5P/o6qgOsuNQz9LwGA04p+BV6JcwoJpjDHSVRxCyV8geUQxKB2
-VakxJgy+kqL4ZIv2Zz6aWQKBgFs/QkA1qzE4a5bnc7V3amw+nBzvFOnidbzJm3HY
-S9c9Km1rquAA0uXwpDzQJnAZre89dOtHjob/1rjrXAuja0k4jseWswwfdr5EhWb1
-rXfJWqPGvmcCjhlwPh1zr5zMaanDiLGtTJP0G52BRqMSjaPesMLQ+vH4nrGhu5Ty
-/v6NAoGBAMeRIQE3p1PYwyzNSBEl+A59sQ2AmOwP1+f50GczPT64I19Nez6I32fS
-k4j0cYxPhgLHZOK0NIPFDlzvw6W0L+/8k+IsTdyvzio+vKcd6Lc7Bn+1H73969cg
-r2x84nfU8u8QsfTvJbeweiBEkMsIgpViTvApC0du3jQCtI1gmPGQ
 -----END RSA PRIVATE KEY-----'''
